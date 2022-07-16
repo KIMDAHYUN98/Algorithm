4ec25a8524cb4ea43d1cf432cb2bc6c1bc9d071b
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2022-07-16 10:34:34 +0900

    Create 배열채우기5.cpp

---
+++ C++/배열/다차원배열/배열채우기5.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int a[5][5];
+int main() 
+{
+	int cnt = 0;
+	int i, j, x=2, y=2;
+	
+	for(i=4; i>=0; i--)
+	{
+		for(j=x; j>=y; j--)
+		{
+			a[j][i] = ++cnt;
+		}
+		if(i>=3) x++, y--;
+		else x--, y++;
+	}
+	
+	for(i=0; i<=4; i++)
+	{
+		for(j=0;  j<=4; j++)
+		{
+			if(a[i][j] == 0) printf("   ");
+			else  printf("%3d", a[i][j]);
+		}
+		printf("\n");
+	}
+
+
+	return 0;
+}
