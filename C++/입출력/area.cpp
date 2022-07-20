f54c5e068638619e1895cd6bca97221533dbd461
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2022-07-20 10:22:30 +0900

    Create area.cpp

---
+++ C++/입출력/area.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main_area ()
+{
+	int a, b, result;
+	scanf("%d %d", &a, &b);
+
+	result = a * b;
+
+	printf("%d", result);
+
+	return 0;
+}
