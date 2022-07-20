ac14dd6da82ba88b4bb194d1bf53f41e1978efd2
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2022-07-20 11:20:04 +0900

    Create swimming.cpp

---
+++ C++/조건문/swimming.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+
+	if (n == 1)
+		printf("enjoy");
+	else if (n == 2)
+		printf("oops");
+	else if (n == 3)
+		printf("enjoy");
+	else if (n == 4)
+		printf("oops");
+	else if (n == 5)
+		printf("enjoy");
+	else if (n == 6)
+		printf("oops");
+	else
+		printf("oops");
+
+	return 0;
+}
