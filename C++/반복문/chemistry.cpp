436f679fa4fa50936cc3da95f50e0126d44acb78
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2022-07-31 18:54:56 +0900

    Update chemistry.cpp

--- C++/반복문/chemistry.cpp
+++ C++/반복문/chemistry.cpp
@@ -1,16 +1,33 @@
 #include<stdio.h>
 int main_chemistry()
 {
-	int i;
-	double a, b=0, t;
+	int i=0;
+	double a, b, c, t;
 
 	while (true)
 	{
 		scanf("%lf", &a);
 		if (a == 999)
 			break;
-		printf("%.2lf", a - b);
-		a = b;
+		
+		if (i == 0)
+		{
+			scanf("%lf", &b);
+			c = b - a;
+			printf("%.2lf\n", c);
+			t = b;
+		}
+		else 
+		{
+			c = a - t;
+			printf("%.2lf\n", c);
+			t = a;
+		}
+
+		i++;
+		
 	}
+
+	printf("End of Output");
 	return 0;
 }
