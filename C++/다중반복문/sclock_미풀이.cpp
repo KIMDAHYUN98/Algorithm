b8de92be6d430b43a12fe26c9f0bd8234c9ff527
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2022-08-03 18:56:43 +0900

    Create sclock_미풀이.cpp

---
+++ C++/다중반복문/sclock_미풀이.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main_sclock()
+{
+	int i, j, n;
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i <= n / 2)
+		{
+			for (j = 0; j < n-i; j++)
+			{
+				if (i <= j)
+					printf("*");
+				else
+					printf(" ");
+			}
+		}
+		else 
+		{
+			for (j = n-1; j >= 0; j--)
+			{
+				if (i>=j)
+					printf("*");
+				else
+					printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
